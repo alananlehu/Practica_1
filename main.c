9611efe019e9abc31cc940bc84cef0bca73fee47
Aram Pena <aram.pena.dse@uteq.edu.mx>
2018-09-07 14:25:19 -0500

    Actualizacion de operaciones

--- main.c
+++ main.c
@@ -3,13 +3,23 @@
 #include <stdlib.h>
 #define RESULTADO 5
 
-int una_resta(int a, int b);
-float division(float a, float b);
+int una_resta(int a, int b)
+{
+	return a - b;
+}
+float division(float a, float b)
+{
+	int salida = 0;
+	if(a > b )
+	{
+		salida = a / b;
+	}
+}
 int una_suma (int a, int b);
 
 int main ()
 {
-printf("Hola Mundo \n");
-division ();
-return o;
+	printf("Hola Mundo \n");
+	division ();
+	return o;
 }
